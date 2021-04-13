ea5395c61aabf0f7fe7200a4fce888f9091e0679
caralocke <cara.m.locke@gmail.com>
2021-04-13 15:33:46 -0400

    Accomplished tasks 1c and 1d on index.js

--- index.js
+++ index.js
@@ -65,6 +65,10 @@ Do the following:
 
    HINT: look up the Number method
 */
+let a = "1999";
+let b = parseInt(a);
+console.log(b);
+
 
 
 
@@ -78,9 +82,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a, b){
+    console.log(a * b)
   }
+  multiply(8, 5);
+  
 
 
 
